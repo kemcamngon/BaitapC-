9b25160a9aa1c85b71c3ee7bed1b3f502a140408
Dang Quynh Trang <kemcamm@gmail.com>
2020-04-23 21:04:42 +0700

    bt6

---
+++ Chuong3_part1_bt6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Chuong3_part1_bt6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DanhSach ds = new DanhSach();
+            ds.Nhap();
+            ds.Xuat();
+            ds.SapXep();
+            Console.Read();
+        }
+    }
+}
